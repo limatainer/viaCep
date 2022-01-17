c2617f9fc2f6a5e3a939016c2ec8fd35a01c23f2
Mariana Cousseiro de Lima <limatainer@MBP-de-Mariana.lan>
2022-01-17 15:55:04 +0000

    readme

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Mapa } from 'assets/img/mapa.svg';
 
 import './styles.css';
 
 const Home = () => {
     return (
         <div className="home-container">
+            <br />
+            <Mapa />
+            <br />
             <Link to="/cepsearch">
                 <button className="btn btn-primary btn-lg start-button">Iniciar</button>
             </Link>
